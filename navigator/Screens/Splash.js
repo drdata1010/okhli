743ce287639159801a26a36f0a3be2aa682de57e
drdata1010 <159292707+drdata1010@users.noreply.github.com>
2024-02-25 10:54:48 +0530

    Update Splash.js

--- navigator/Screens/Splash.js
+++ navigator/Screens/Splash.js
@@ -6,7 +6,7 @@ const Splash = () => {
   const navigation = useNavigation();
   useEffect(() => {
     setTimeout(() => {
-      navigation.navigate("Splash");
+      navigation.navigate("Login");
     }, 3000)
   }, [])
   return (
